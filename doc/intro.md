25a3c30cb52c916a96a2e6dc2ddb7c081623838b
Sergiy BONDARYEV <sbondaryev@gmail.com>
2015-10-20 23:08:55 +0200

    init commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ca-recipe
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ca_recipe/core_test.clj
@@ -0,0 +1,7 @@
+(ns ca-recipe.core-test
+  (:require [clojure.test :refer :all]
+            [ca-recipe.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
