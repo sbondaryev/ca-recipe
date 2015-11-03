9271a1d1710c053be6f4326b422f55b2c7f4db99
Sergiy BONDARYEV <sbondaryev@gmail.com>
2015-11-03 21:36:27 +0100

    add pair implementation

---
+++ src/ca_recipe/pair.clj
@@ -0,0 +1,25 @@
+(ns ca-recipe.pair
+  (import [clojure.lang Counted Indexed ILookup Seqable]))
+
+(deftype Pair [a b]
+  Seqable
+  (seq [_] (seq [a b]))
+
+  Counted
+  (count [_] 2)
+
+  Indexed
+  (nth [_ i]
+    (case i
+      0 a
+      1 b
+      (throw (IllegalArgumentException.))))
+  (nth [this i _] (nth this i))
+
+  ILookup
+  (valAt [_ k _]
+    (case k
+      0 a
+      1 b
+      (throw (IllegalArgumentException.))))
+  (valAt [this k] (.valAt this k nil)))
