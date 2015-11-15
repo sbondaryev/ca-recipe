6af784d21aa7a5b5306fcdb1681c727580460bba
Sergiy BONDARYEV <sbondaryev@gmail.com>
2015-11-15 21:54:32 +0100

    add atom to stock items

--- src/ca_recipe/store.clj
+++ src/ca_recipe/store.clj
@@ -1,7 +1,40 @@
 (ns ca-recipe.store)
 
 (defn go-shopping-naive
-  "Returns a list of items purchased" [shopping-list]
+  "Returns a list of items purchased"
+  [shopping-list]
   (loop [[item & items] shopping-list
-         cart []] (if item
-                    (recur items (conj cart item)) cart)))
+         cart []]
+    (if item
+      (recur items (conj cart item))
+      cart)))
+
+(def inventory (arom {}))
+
+(defn no-negative-values?
+  "check values of a map for a negative value"
+  [m]
+  (not-any? neg? (vals m)))
+
+(defn in-stock?
+  "check if an item is in stock"
+  [item]
+  (let [cnt (item @inventory)]
+    (and (pos? cnt))))
+
+(defn init
+  "set up store with inventory"
+  [items]
+  (set-validator! inventory no-negative-values?)
+  (swap! inventory items))
+
+(defn grab
+  "grab an item from the shelves"
+  [item]
+  (if (in-stock? item)
+    (swap! inventory unpdate-in [item] dec)))
+
+(defn stock
+  "stock an item on the shelves"
+  [item]
+  (swap! inventory update-in [item] inc))
