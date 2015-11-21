f065beb0c3ada6c5c488181598480f4a1e8d0ceb
Sergiy BONDARYEV <sbondaryev@gmail.com>
2015-11-21 21:57:26 +0100

    indent code

--- src/ca_recipe/family_async.clj
+++ src/ca_recipe/family_async.clj
@@ -1,6 +1,9 @@
 (ns ca-recipe.family-async)
 
-(def shopping-list (ref #{})) (def assignments (ref {})) (def shopping-cart (ref #{}))
+(def shopping-list (ref #{}))
+(def assignments (ref {}))
+(def shopping-cart (ref #{}))
+
 (defn init []
   (store/init {:eggs 2 :bacon 3 :apples 3
                :candy 5 :soda 2 :milk 1
@@ -8,5 +11,6 @@
                :cheese 3})
   (dosync
    (ref-set shopping-list #{:milk :butter :bacon :eggs
-                            :carrots :potatoes :cheese :apples}) (ref-set assignments {})
+                            :carrots :potatoes :cheese :apples})
+   (ref-set assignments {})
    (ref-set shopping-cart #{})))
