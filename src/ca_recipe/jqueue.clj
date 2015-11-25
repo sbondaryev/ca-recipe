c162b4a93a7527d7b0123700a22f9cacec3876eb
Sergiy BONDARYEV <sbondaryev@gmail.com>
2015-11-25 22:15:55 +0100

    add java queue

---
+++ src/ca_recipe/jqueue.clj
@@ -0,0 +1,25 @@
+(ns ca-recipe.jqueue
+  (:import [java.util.concurrent LinkedBlockingQueue]))
+
+(defn pusher [q n]
+  (loop [i 0]
+    (when (< i n)
+      (.put q i)
+      (recur (inc i))))
+  (.put q :END))
+
+(defn popper [q]
+  (loop [items []]
+    (let [item (.take q)]
+      (if (= item :END)
+        item
+        (recur (conj items item))))))
+
+(defn flow [n]
+  (let [q (LinkedBlockingQueue.)
+        consumer (future (papper q))
+        begin (System/currentTimeMillis)
+        producer (future (pusher q n))
+        received @cunsumer
+        end (System/currentTimeMillis)]
+    (println "Received:" (count received) "in" (- end begin) "ms")))
