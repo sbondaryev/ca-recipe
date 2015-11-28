6745278313969d01a3746a920fc33608484a1ccb
Sergiy BONDARYEV <sbondaryev@gmail.com>
2015-11-28 22:39:00 +0100

    api refacto

--- src/ca_recipe/components/ke.clj
+++ src/ca_recipe/components/ke.clj
@@ -1,13 +1,28 @@
 (ns ca-recepie.components.ke)
 
-;; read
-(defn get-rules [ke])
-(defn find-rules [ke criterial])
+(defprotocol KE
+  (get-rules [ke] "Get full rule set")
+  (transform-rules [ke update-fn]
+                   "Apply transformation function to rule set. Return new KE")
+  (fire-rules [ke request]
+              "Fire the rules against the request and return a response"))
 
-;;update
-(defn add-rule [ke rule])
-(defn replace-rule [ke old-rule new-role])
-(defn delete-rule [ke rule])
+;; private functions
+(defn- transform-criteria [criteria])
 
-;;processing
-(defn fire-rules [ke request])
+;; api build over the protocol
+(defn find-rules
+  [ke criteria]
+  (filter (transform-criteria criteria) (get-rules ke)))
+
+(defn add-rule
+  [ke rule]
+  (transform-rules ke #(conj % rule)))
+
+(defn replace-rule
+  [ke old-rule new-rule]
+  (transform-rules ke #(-> % (disj old-rule) (conj new-rule))))
+
+(defn delete-rule
+  [ke rule]
+  (tranform-rules ke #(-> % (disj rule))))
