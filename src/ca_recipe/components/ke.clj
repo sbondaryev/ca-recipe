350bdb36200652e923634ac8fe06bf9abd1dbf41
Sergiy BONDARYEV <sbondaryev@gmail.com>
2015-11-28 22:09:15 +0100

    complete interface

--- src/ca_recipe/components/ke.clj
+++ src/ca_recipe/components/ke.clj
@@ -1,4 +1,13 @@
 (ns ca-recepie.components.ke)
 
+;; read
 (defn get-rules [ke])
 (defn find-rules [ke criterial])
+
+;;update
+(defn add-rule [ke rule])
+(defn replace-rule [ke old-rule new-role])
+(defn delete-rule [ke rule])
+
+;;processing
+(defn fire-rules [ke request])
