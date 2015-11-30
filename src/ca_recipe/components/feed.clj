66d2bb269e0f43c0766709199fe3334bdd9d30d8
Sergiy BONDARYEV <sbondaryev@gmail.com>
2015-11-30 22:07:14 +0100

    add feed component

---
+++ src/ca_recipe/components/feed.clj
@@ -0,0 +1,13 @@
+(ns ca-recipe.components.feed
+  :require [com.stuartsierra.component :as component])
+
+(defrecord Feed [auth status msg-chan response-chan]
+  component/Lifecicle
+  (start [component]
+    (reset! (:status component) :running)
+    (process-messages status msg-chan)
+    (handle-responses status response-chan)
+    component)
+  (stop [component]
+    (reset! (:status component) :stopoed)
+    component))
