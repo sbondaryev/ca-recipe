d2a25c8f262e77e1293c592764fcd50b93d07630
Sergiy BONDARYEV <sbondaryev@gmail.com>
2015-11-30 22:25:39 +0100

    add knowledge engine

---
+++ src/ca_recipe/components/kengine.clj
@@ -0,0 +1,17 @@
+(defrecord KnowledgeEngine
+    [ke-config feed-chan alert-chan rules]
+  component/Ligecicle
+  (start [component]
+    (watch-feeds feed-chan alert-chan)
+    component)
+  (stop [component]
+    component))
+
+(defn new-knowledge-engine
+  [ke-config feed-chan alert-chan]
+  (->KnowledgeEngine ke-config feed-chan alert-chan
+                     (atom (:rule-set ke-config))))
+
+(defn add-rule
+  [ke rule]
+  (swap! (:rules ke) conj rule))
