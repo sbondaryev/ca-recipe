edc8fae5cc948a18210b5adea0aaab0db31612b3
Sergiy BONDARYEV <sbondaryev@gmail.com>
2015-12-06 23:41:34 +0100

    indent view

--- src/ca_recipe/tnywb/view.clj
+++ src/ca_recipe/tnywb/view.clj
@@ -1,8 +1,16 @@
 (ns mbfpp.oo.tinyweb.steptwo
   (:import (com.mblinn.oo.tinyweb RenderingException)))
-(defn test-controller [http-request] {:name (http-request :body)})
-(defn test-view [model]
+
+(defn test-controller
+  [http-request]
+  {:name (http-request :body)})
+
+(defn test-view
+  [model]
   (str "<h1>Hello, " (model :name) "</h1>"))
-(defn- render [view model] (try
-                             (view model)
-                             (catch Exception e (throw (RenderingException. e)))))
+
+(defn- render
+  [view model]
+  (try
+    (view model)
+    (catch Exception e (throw (RenderingException. e)))))
