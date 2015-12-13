9a0a769b85848eaaa34bc0fde20e776179d3f87d
Sergiy BONDARYEV <sbondaryev@gmail.com>
2015-12-13 22:36:21 +0100

    view part

--- src/ca_recipe/tnywb/core.clj
+++ src/ca_recipe/tnywb/core.clj
@@ -34,3 +34,12 @@
 
 (defn handle-greeting [http-request]
   {:greetings (map make-greeting (str/split (:body http-request) #","))})
+
+(defn render-greeting [greeting]
+  (str "<h2>"greeting"</h2>"))
+
+(defn greeting-view [model]
+  (let [rendered-greetings (str/join " " (map render-greeting (:greetings model)))]
+    (str "<h1>Friendly Greetings</h1>" rendered-greetings)))
+
+(defn logging-filter [http-request]
