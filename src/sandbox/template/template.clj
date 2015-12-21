b9983dca564ce26a10e459ae857e6061b805ff01
Sergiy BONDARYEV <sbondaryev@gmail.com>
2015-12-21 21:35:53 +0100

    add +/- grade template implementation

--- src/sandbox/template/template.clj
+++ src/sandbox/template/template.clj
@@ -24,3 +24,24 @@
 (def full-grade-reporter (make-grade-reporter full-grade-converter print-histogram))
 
 (def sample-grades [5.0 4.0 4.4 2.2 3.3 3.5])
+
+(defn plus-minus-grade-converter [grade]
+  (cond
+    (and (<= grade 5.0) (> grade 4.7)) "A"
+    (and (<= grade 4.7) (> grade 4.3)) "A-"
+    (and (<= grade 4.3) (> grade 4.0)) "B+"
+    (and (<= grade 4.0) (> grade 3.7)) "B"
+    (and (<= grade 3.7) (> grade 3.3)) "B-"
+    (and (<= grade 3.3) (> grade 3.0)) "C+"
+    (and (<= grade 3.0) (> grade 2.7)) "C"
+    (and (<= grade 2.7) (> grade 2.3)) "C"
+    (and (<= grade 2.3) (> grade 0)) "D"
+    (= grade 0) "F" :else "N/A"))
+
+(defn print-all-grades [grades]
+  (doseq [grade grades]
+    (println "Grade is:" grade)))
+
+(def plus-minus-grade-repoerter
+  (make-grade-reporter plus-minus-grade-converter print-all-grades))
+   
