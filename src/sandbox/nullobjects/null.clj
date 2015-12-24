ce6004bd65700e0fecf6dbb477759cab089601e1
Sergiy BONDARYEV <sbondaryev@gmail.com>
2015-12-24 12:19:56 +0100

    indent + examples

--- src/sandbox/nullobjects/null.clj
+++ src/sandbox/nullobjects/null.clj
@@ -1,4 +1,9 @@
 (ns sandbox.nullobjects.null)
 
-(defn build-person [first-name last-name] (if (and first-name last-name)
-                                            {:first-name first-name :last-name last-name} {:first-name "John" :last-name "Doe"}))
+(defn build-person [first-name last-name]
+  (if (and first-name last-name)
+    {:first-name first-name :last-name last-name}
+    {:first-name "John" :last-name "Doe"}))
+
+(def p1 (build-person "Fname" "Sname"))
+(def nullperson (build-person "Fname" nil))
