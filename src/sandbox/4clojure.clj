680ca1e3624179165fa2ea7829b6356a8ef3fba6
Sergiy BONDARYEV <sbondaryev@gmail.com>
2015-12-24 12:28:07 +0100

    4clojure solvings (1-10)

---
+++ src/sandbox/4clojure.clj
@@ -0,0 +1,31 @@
+(ns sandbox.4clojure)
+
+;;1 Nothing but the Truth
+true
+
+;;2 Simple Math
+4
+
+;;3 Intro to Strings
+"HELLO WORLD"
+
+;;4 Intro to Lists
+:a :b :c
+
+;;5 Lists: conj
+'(1 2 3 4)
+
+;;6 Intro to Vectors
+:a :b :c
+
+;;7 Vectors: conj
+[1 2 3 4]
+
+;;8 Intro to Sets
+#{:a :b :c :d}
+
+;;9 Sets: conj
+2
+
+;;10 Intro to Maps
+20
