93140be50048612448d0c2d38385af0ce11ad7ab
Sergiy BONDARYEV <sbondaryev@gmail.com>
2016-01-09 20:43:29 +0100

    add longest match

--- src/sandbox/lz77.clj
+++ src/sandbox/lz77.clj
@@ -13,5 +13,15 @@
              (into %1 (expand %1 %2)))
           [] xs))
 
+(defn all-subvecs-from-beginning [xs]
+  (map #(take % xs) (range 1 (inc (count xs)))))
+
 (defn all-subvecs [xs]
-  (mapcat #(partition % 1 xs) (range 1 (count xs))))
+  (for [i (range (inc (count xs))) k (range i)]
+    [(drop k (take i xs)) k]))
+
+(defn longest-match-w-beginning [left-array right-array]
+  (let [all-left-chunks (all-subvecs left-array)
+        all-right-chunks-from-beginning (all-subvecs-from-beginning right-array)]
+    (filter #(some #{(first %)} all-right-chunks-from-beginning) all-left-chunks)))
+        
