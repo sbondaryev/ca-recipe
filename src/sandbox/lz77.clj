c253dadc3f5251d670c42af93ec6fe93f0b408e5
Sergiy BONDARYEV <sbondaryev@gmail.com>
2016-01-09 11:23:14 +0100

    unlz77 refacto

--- src/sandbox/lz77.clj
+++ src/sandbox/lz77.clj
@@ -2,22 +2,13 @@
   (:require [clojure.set :as cset]))
 
 
-(defn expand
-  [the-vector
-   distance
-   length]
+(defn expand [the-vector [distance length]]
   (->> (take-last distance the-vector)
        (cycle)
        (take length)))
 
-(defn un-LZ77
-  [bytes]
-  (loop [result []
-         remaining bytes]
-    (if (seq remaining)
-      (let [current (first remaining)
-            the-rest (rest remaining)]
-        (if-not (vector? current)
-          (recur (conj result current) the-rest)
-          (recur (into result (expand result (current 0) (current 1))) the-rest)))
-      result)))
+(defn un-LZ77 [bites]
+  (reduce #(if-not (vector? %2)
+             (conj %1 %2)
+             (into %1 (expand %1 %2)))
+          [] bites))
