e89e2bf180ec4d2dc9380ba66ce3fdaa63bc37b2
Sergiy BONDARYEV <sbondaryev@gmail.com>
2016-01-18 22:23:02 +0100

    genarate program, fire-a-process

--- src/sandbox/multithread.clj
+++ src/sandbox/multithread.clj
@@ -9,12 +9,9 @@
 "S = INSTRS
   INSTRS = ((INSTR | LOCKED-INSTRS) <optional-whitespace>)*
   INSTR = HEAVY-OP | MEDIUM-OP | LIGHT-OP
-  HEAVY-OP = <optional-whitespace> 'heavy-op' <whitespace> ID
-<SEP>
-  MEDIUM-OP = <optional-whitespace> 'medium-op' <whitespace> ID
-<SEP>
-  LIGHT-OP = <optional-whitespace> 'light-op' <whitespace> ID
-<SEP>
+  HEAVY-OP = <optional-whitespace> 'heavy-op' <whitespace> ID <SEP>
+  MEDIUM-OP = <optional-whitespace> 'medium-op' <whitespace> ID <SEP>
+  LIGHT-OP = <optional-whitespace> 'light-op' <whitespace> ID <SEP>
   
   LOCKED-INSTRS = LOCK INSTRS UNLOCK
   LOCK = <optional-whitespace> 'lock' <whitespace> ID <SEP>
@@ -26,3 +23,37 @@
   whitespace = #'\\s+'
   optional-whitespace = #'\\s*'
   SEP = #'\\s*' ';'")
+
+(defn gen-program [parser program]
+  (insta/transform
+   {:S identity
+    :INSTRS (fn [& args] (vec args))
+    :INSTR identity
+    :HEAVY-OP (fn [x y] {:inst-type :heavy-op :inst-id (get y 1)})
+    :MEDIUM-OP (fn [x y] {:inst-type :medium-op :inst-id (get y 1)})
+    :LIGHT-OP (fn [x y] {:inst-type :light-op :inst-id (get y 1)})
+    :LOCKED-INSTRS (fn [& args] (vec args))
+    :LOCK (fn [x y] {:inst-type :lock :inst-id {:lock (get y 1)}})
+    :UNLOCK (fn [x y] {:inst-type :unlock :inst-id {:unlock  (get y 1)}})}
+   (parser program)))
+
+(defn fire-a-process [grammar program process-id priority]
+  (let [prsr (insta/parser grammar)
+        vec-instructions (gen-program prsr program)
+;;=> the nested structure
+        zpr (z/vector-zip vec-instructions)]
+    (loop [result []
+           loc (->  zpr z/down)]
+      (if (z/end? loc)
+;;=> the end of recursion, no more nodes to visit
+        {:process-id process-id
+         :instructions result
+         :priority priority}
+;;=> We generate the process
+        (recur (if (map? (z/node loc))
+;;=> We only append to result the elements of type 'map'
+                 (conj result (z/node loc))
+                 result)
+;;=> else we pass result as is in the recursion
+               (z/next loc))))))
+;;=> and we recur with the next element.
