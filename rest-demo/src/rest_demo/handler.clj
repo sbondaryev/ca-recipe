8fffe9a3d1c2a4d7c9add4e50ac55d534d95a368
Sergiy BONDARYEV <sbondaryev@gmail.com>
2016-02-03 21:34:30 +0100

    simple rest routing

--- rest-demo/src/rest_demo/handler.clj
+++ rest-demo/src/rest_demo/handler.clj
@@ -3,9 +3,19 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(defn handle-http []
+  (context "/:id" [id]
+           (defroutes api-routes
+             (GET "/" [] (str "get called: " id "\n"))
+             (POST "/" {form-params :form-params} (str "post called: " id "\n" form-params " \n"))
+             (PUT "/" req (str "put called with params: " req))
+             (DELETE "/" [] (str "delete called: " id "\n")))))
+
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/not-found "Not Found"))
+  (handle-http)
+  (route/not-found (str "This is the default page - try"
+                        "<a href='http://localhost:4000/33'>this</a>")))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (wrap-defaults app-routes
+                 (assoc-in site-defaults [:security :anti-forgery] false)))
