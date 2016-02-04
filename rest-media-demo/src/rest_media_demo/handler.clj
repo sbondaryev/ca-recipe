7f2640dd9325beee3eabe15b2dcc53ece4885746
Sergiy BONDARYEV <sbondaryev@gmail.com>
2016-02-04 22:01:11 +0100

    configure namespaces

--- rest-media-demo/src/rest_media_demo/handler.clj
+++ rest-media-demo/src/rest_media_demo/handler.clj
@@ -1,7 +1,12 @@
 (ns rest-media-demo.handler
-  (:require [compojure.core :refer :all]
+  (:use compojure.core)
+  (:require [compojure.handler :as handler]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [liberator.core :refer [resource defresource]]
+            [liberator.representation :refer [render-map-generic]]
+            [hiccup.core :refer [html]]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [clojure.data.json :as json]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
