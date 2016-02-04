942458881672fc1d77dc1b206c8a7b0c0490491e
Sergiy BONDARYEV <sbondaryev@gmail.com>
2016-02-04 22:00:58 +0100

    set project environment

--- rest-media-demo/project.clj
+++ rest-media-demo/project.clj
@@ -1,12 +1,14 @@
 (defproject rest-media-demo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]]
+                 [ring/ring-defaults "0.1.5"]
+                 [liberator "0.13"]
+                 [org.clojure/data.json "0.2.6"]]
   :plugins [[lein-ring "0.9.7"]]
-  :ring {:handler rest-media-demo.handler/app}
+  :ring {:handler rest-media-demo.handler/app
+         :auto-reload? true
+         :auto-refresh? true}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.0"]]}})
