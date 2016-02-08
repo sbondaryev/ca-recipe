57f9b022a6feebd745a0a620ada98012bbfa1d91
Sergiy BONDARYEV <sbondaryev@gmail.com>
2016-02-08 23:04:46 +0100

    modify project configuration

--- cljs-rest-client-server-demo/project.clj
+++ cljs-rest-client-server-demo/project.clj
@@ -1,11 +1,34 @@
 (defproject cljs-rest-client-server-demo "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.228"]]
-  :jvm-opts ^:replace ["-Xmx1g" "-server"]
-  :plugins [[lein-npm "0.6.1"]]
+                 [org.clojure/clojurescript "1.7.228"]
+                 [ring/ring-core "1.4.0"]
+                 [ring/ring-json "0.4.0"]]
+  ;;:jvm-opts ^:replace ["-Xmx1g" "-server"]
+  :plugins [[lein-npm "0.6.1"]
+            [lein-cljsbuild "1.1.2"]
+            [lein-ring "0.9.7"]]
+  :ring {:handler cljs-rest-client-server-demo.handler/app}
   :npm {:dependencies [[source-map-support "0.4.0"]]}
   :source-paths ["src" "target/classes"]
-  :clean-targets ["out" "release"]
-  :target-path "target")
+  :clean-targets ["out/cljs_rest_client_server_demo"
+                  "cljs_rest_client_server_demo.js"
+                  "cljs_rest_client_server_demo.min.js"]
+  ;;:target-path "target"
+  :cljsbuild {
+              :builds [{:id "dev"
+                        :source-paths ["src-cljs"]
+                        :compiler {
+                                   :main cljs-rest-client-server-demo.core
+                                   :output-to "out/cljs_rest_client_server_demo.js"
+                                   :output-dir "out"
+                                   :optimizations :none
+                                   :cache-analysis true
+                                   :source-map true}}
+                       {:id "release"
+                        :source-paths ["src-cljs"]
+                        :compiler {
+                                   :main cljs-rest-client-server-demo.core
+                                   :output-to "out-adv/cljs_rest_client_server_demo.min.js"
+                                   :output-dir "out-adv"
+                                   :optimizations :advanced
+                                   :pretty-print false}}]})
