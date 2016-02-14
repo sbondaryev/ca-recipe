a46e6f352f8ee3532d21353340c7d2a659ca89ff
Sergiy BONDARYEV <sbondaryev@gmail.com>
2016-02-14 21:19:52 +0100

    config project

--- json-demo/project.clj
+++ json-demo/project.clj
@@ -1,10 +1,9 @@
 (defproject json-demo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]]
+                 [ring/ring-defaults "0.1.5"]
+                 [ring/ring-json "0.3.1"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler json-demo.handler/app}
   :profiles
