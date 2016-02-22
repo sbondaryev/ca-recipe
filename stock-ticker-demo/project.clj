97aa0f0a182793fea61f988ce4c8ba19f9e310e1
Sergiy BONDARYEV <sbondaryev@gmail.com>
2016-02-22 22:16:34 +0100

    modify project

--- stock-ticker-demo/project.clj
+++ stock-ticker-demo/project.clj
@@ -1,8 +1,4 @@
 (defproject stock-ticker-demo "0.0.1-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [io.pedestal/pedestal.service "0.4.1"]
 
@@ -15,7 +11,9 @@
                  [ch.qos.logback/logback-classic "1.1.3" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/jul-to-slf4j "1.7.12"]
                  [org.slf4j/jcl-over-slf4j "1.7.12"]
-                 [org.slf4j/log4j-over-slf4j "1.7.12"]]
+                 [org.slf4j/log4j-over-slf4j "1.7.12"]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/core.async "0.2.374"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "stock-ticker-demo.server/run-dev"]}
