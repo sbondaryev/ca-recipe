d9013e4397abf40c3d777e1c4b3969964014f1d9
Sergiy BONDARYEV <sbondaryev@gmail.com>
2016-02-24 23:10:24 +0100

    server side

--- stock-ticker-demo/project.clj
+++ stock-ticker-demo/project.clj
@@ -1,13 +1,7 @@
 (defproject stock-ticker-demo "0.0.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [io.pedestal/pedestal.service "0.4.1"]
-
-                 ;; Remove this line and uncomment one of the next lines to
-                 ;; use Immutant or Tomcat instead of Jetty:
                  [io.pedestal/pedestal.jetty "0.4.1"]
-                 ;; [io.pedestal/pedestal.immutant "0.4.1"]
-                 ;; [io.pedestal/pedestal.tomcat "0.4.1"]
-
                  [ch.qos.logback/logback-classic "1.1.3" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/jul-to-slf4j "1.7.12"]
                  [org.slf4j/jcl-over-slf4j "1.7.12"]
--- stock-ticker-demo/src/stock_ticker_demo/service.clj
+++ stock-ticker-demo/src/stock_ticker_demo/service.clj
@@ -4,12 +4,48 @@
             [io.pedestal.http.body-params :as body-params]
             [io.pedestal.http.route.definition :refer [defroutes]]
             [ring.util.response :as ring-resp]
-            [io.pedestal.http.see :as sse]
+            [io.pedestal.http.sse :as sse]
             [clojure.data.json :as json]
             [stock-ticker-demo.price-feed :as price-feed]
             [clojure.core.async :as async])
   (:import [java.util Date]
            [java.util.concurrent Executors]))
+
+
+(defn pump-stock-prices
+  "Given a count - iterate returning a series of stock prices"
+  [event-ch num-iterations ctx]
+  (let [stock-code (rand-nth price-feed/stock-codes)
+        price (price-feed/curr-price stock-code)
+        stock-info (price-feed/stock-map stock-code)
+        eps (:eps stock-info)]
+    (async/put! event-ch {:name "price"
+                          :data (json/write-str
+                                 {:stock-code stock-code :price price
+                                  :eps eps})})
+    (println
+     (str stock-code " " price " " eps " " stock-info " "
+          (java.util.Date.) "\n")))
+  (Thread/sleep 1000)
+  (if (> num-iterations 0)
+    (recur event-ch (dec num-iterations) ctx)
+    (do
+      (async/put! event-ch {:name "close" :data ""})
+      (async/close! event-ch))))
+
+(defn prices-stream
+  "Starts sending price events to the channel"
+  [event-ch ctx]
+  (pump-stock-prices event-ch 100 ctx))
+
+(defn prices-called
+  "Feedback the when a stock order is placed on the web page"
+  [{{order-json "order"} :form-params}]
+  (let [order (json/read-str order-json :key-fn keyword)
+        {stock-code :stock-code price :price} order]
+    (println
+     (str "An order has come in for: " stock-code " at: " price)))
+  {:status 204})
 
 (defn about-page
   [request]
@@ -22,36 +58,15 @@
   (ring-resp/response "Hello World!"))
 
 (defroutes routes
-  ;; Defines "/" and "/about" routes with their associated :get handlers.
-  ;; The interceptors defined after the verb map (e.g., {:get home-page}
-  ;; apply to / and its children (/about).
   [[["/" {:get home-page}
      ^:interceptors [(body-params/body-params) bootstrap/html-body]
+     ["/prices" {:get [::prices (sse/start-event-stream prices-stream)]
+                 :post prices-called}]
      ["/about" {:get about-page}]]]])
 
-;; Consumed by stock-ticker-demo.server/create-server
-;; See bootstrap/default-interceptors for additional options you can configure
+
 (def service {:env :prod
-              ;; You can bring your own non-default interceptors. Make
-              ;; sure you include routing and set it up right for
-              ;; dev-mode. If you do, many other keys for configuring
-              ;; default interceptors will be ignored.
-              ;; ::bootstrap/interceptors []
               ::bootstrap/routes routes
-
-              ;; Uncomment next line to enable CORS support, add
-              ;; string(s) specifying scheme, host and port for
-              ;; allowed source(s):
-              ;;
-              ;; "http://localhost:8080"
-              ;;
-              ;;::bootstrap/allowed-origins ["scheme://host:port"]
-
-              ;; Root for resource interceptor that is available by default.
               ::bootstrap/resource-path "/public"
-
-              ;; Either :jetty, :immutant or :tomcat (see comments in project.clj)
               ::bootstrap/type :jetty
-              ;;::bootstrap/host "localhost"
-              ::bootstrap/port 8080})
-
+              ::bootstrap/port 8080}) 
