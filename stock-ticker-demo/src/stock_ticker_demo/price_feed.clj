8df7cdb33046e10b7edb105d29889b96178252d4
Sergiy BONDARYEV <sbondaryev@gmail.com>
2016-02-24 23:11:02 +0100

    fixes

--- stock-ticker-demo/src/stock_ticker_demo/price_feed.clj
+++ stock-ticker-demo/src/stock_ticker_demo/price_feed.clj
@@ -3,7 +3,7 @@
 (def stock-map {"YHOO" {:max-price 31.1 :min-price 11.51
                         :wavelength 90 :starting-point 0.5
                         :eps 2.15}
-                "APPL" {:max-price 408.38 :min-price 84.11
+                "AAPL" {:max-price 408.38 :min-price 84.11
                         :wavelength 60 :starting-point 0
                         :eps 31.25}
                 "GOOG" {:max-price 809.1 :min-price 292.96
