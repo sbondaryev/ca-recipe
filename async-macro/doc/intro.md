4fd220a7b85cf9a082eabd832982ffaf77bfe285
Sergiy BONDARYEV <sbondaryev@gmail.com>
2016-03-06 21:47:21 +0100

    async macro

---
+++ async-macro/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to async-macro
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ async-macro/test/async_macro/core_test.clj
@@ -0,0 +1,7 @@
+(ns async-macro.core-test
+  (:require [clojure.test :refer :all]
+            [async-macro.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
