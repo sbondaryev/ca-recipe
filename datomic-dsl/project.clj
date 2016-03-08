234143df28494874d4f351c3d9d3baaa41bd7cb1
Sergiy BONDARYEV <sbondaryev@gmail.com>
2016-03-08 22:09:41 +0100

    configure project

--- datomic-dsl/project.clj
+++ datomic-dsl/project.clj
@@ -1,9 +1,6 @@
 (defproject datomic-dsl "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.datomic/datomic-free "0.9.5350" :exclusions [joda-time]]]
   :main ^:skip-aot datomic-dsl.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
