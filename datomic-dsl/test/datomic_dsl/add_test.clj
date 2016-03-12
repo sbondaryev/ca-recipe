3f99a6a2d2020a68d87e51c9d66624ba31b49b8f
Sergiy BONDARYEV <sbondaryev@gmail.com>
2016-03-12 23:26:30 +0100

    add tests

---
+++ datomic-dsl/test/datomic_dsl/add_test.clj
@@ -0,0 +1,28 @@
+;;lein test :only datomic-dsl.add-test
+(ns datomic-dsl.add-test
+  (:require [clojure.test :refer :all]
+            [datomic-dsl.add :refer :all]
+            [datomic-dsl.common-test :refer :all]
+            [datomic-dsl.create-test :refer :all]
+            [clojure.pprint :refer :all]
+            [datomic.api :as d]))
+
+;; (pprint book-add)
+
+(def add-result
+  "Keep a reference to the transaction result if we need to look at it later."
+  (d/transact conn book-add))
+
+(def book-datom
+  "Qurey against the result. Full sotp in the query to find the first one."
+  (d/touch
+   (d/entity
+    (d/db conn)
+    (d/q '[:find ?e . :where [?e :book/author]]
+         (d/db conn)))))
+
+;; (pprint book-datom)
+
+(deftest datom-added-test
+  (testing "Is the datom in the database?"
+    (is (= 2 (count book/datom)))))
