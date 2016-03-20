45b58fe29c9dfa25bbae20992c5832988ec5993c
Sergiy BONDARYEV <sbondaryev@gmail.com>
2016-03-20 22:32:05 +0100

    dsl processing funcs

--- datomic-dsl/src/datomic_dsl/add_nested.clj
+++ datomic-dsl/src/datomic_dsl/add_nested.clj
@@ -32,3 +32,52 @@
         (zip-nodes
          dsl-zipper dsl))))
 
+(defn get-key-sequences
+  "Get each of the nested nodes as a flat vector of nodes to map over."
+  [nested-dsl]
+  (let [nested-datom-dsl-zipper (map-zip nested-dsl)]
+    (map vec (map-keys-seqs nested-dsl))))
+
+(defn append-in
+  "Given a key sequence - append a value at the nested location."
+  [nested-coll nested-key-seq param]
+  (assoc-in nested-coll nested-key-seq
+            (conj (get-in nested-coll nested-key-seq) param)))
+
+(defn replace-children
+  "For a map of maps - replace the children maps with their nested dbids."
+  [[k v]]
+  (if (= clojure.lang.PersistentHashMap (class v))
+    [k (:db/id v)]
+    [k v]))
+
+(defn add-matching-dbids-to-nested-child
+  "Map this onto each of the nested children using a key-sequence to give them a dbid."
+  [nest-datom-dsl-input current-seq]
+  (let [current-seq-rev (reverse current-seq)
+        parent-seq (drop-last current-seq)
+        last-two (take-last 2 current-seq-rev)
+        dbid (d/tempid :db.part/user)
+        child-dbid (:db/id dbid)
+        parent-keyword (case (count last-two)
+                         2 (keyword (first last-two) (second last-two))
+                         1 (kayword (first last-two)))
+        parent-dbid (if (> (count curent-seq) 1) (parent-keyword dbid))
+        child-replace (append-in nest-datom-dsl-input current-seq-vec child-dbid)
+        parent-replace (marge child-replace parent-dbid)]
+    parent-replace))
+
+(defn key-sequence
+  "Get the key sequence from the dsl and filter out the top-level ones and return a set of vectors."
+  [dsl]
+  (set
+   (map vec
+        (map rest
+             (filter #(> (count %) 1)
+                     (get-key-sequences dsl))))))
+
+(defn nested-insert-dsl
+  "Step through the existing dsl and add matching dbids to future parent and children entities."
+  [schema-name dsl]
+  (reduce add-matching-dbids-to-nested-child
+          dsl (key-sequence dsl)))
