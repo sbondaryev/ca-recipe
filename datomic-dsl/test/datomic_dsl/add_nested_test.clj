76a7f0333fe5e0342466bf62143b34c65c3cbb76
Sergiy BONDARYEV <sbondaryev@gmail.com>
2016-03-23 23:01:26 +0100

    added nested tests

--- datomic-dsl/test/datomic_dsl/add_nested_test.clj
+++ datomic-dsl/test/datomic_dsl/add_nested_test.clj
@@ -14,4 +14,27 @@
             "author" "Charles Nordoff"}
     "borrower" {"name" "John Smith"}
     "date" "today"}})
-   
+
+(def nested-datomic-add-syntax
+  "Convert data structure to datomic syntax."
+  (convert-dsl-to-datomic-syntax "borrowevent" nest-datom-dsl))
+
+;;(pprint nested-datomic-add-syntax)
+
+(def add-result
+  "Keep a reference to the transaction result"
+  (d/transact conn nested-datomic-add-syntax))
+
+(def borrowed-datom
+  "Query against the result"
+  (d/touch
+   (d/entity
+    (d/db conn)
+    (d/q '[:find ?e . :where [?e :borrower/name]]
+         (d/db conn)))))
+
+;;(pprint borrowed-datom)
+
+(deftest datom-added-test
+  (testing "Is the datom added in the database?"
+    (is (= 1 (count borrower-datom)))))
