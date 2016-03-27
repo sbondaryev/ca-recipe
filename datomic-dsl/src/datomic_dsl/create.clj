1fe44694d56bed81d070b78c8cda1b9772ba7297
Sergiy BONDARYEV <sbondaryev@gmail.com>
2016-03-27 23:08:07 +0200

    fix db request

--- datomic-dsl/src/datomic_dsl/create.clj
+++ datomic-dsl/src/datomic_dsl/create.clj
@@ -3,16 +3,14 @@
    [datomic.api :as d]))
 
 (defn create-schema
-  "Given a schema name and some schema attribute key-value pairs,
-  create an insertable dotomic schema."
+  "Given a schema name and some schema attribute key-value pairs, create an insertable datomic schema."
   [schema-name & attributes]
   (let [fields (apply hash-map attributes)]
     (mapcat (fn [[attribute-name attribute-type]]
               [{:db/id (d/tempid :db.part/db)
                 :db/ident (keyword schema-name attribute-name)
                 :db/valueType (keyword "db.type" attribute-type)
-                :db/carfinality :db.cardiability/one
+                :db/cardinality :db.cardinality/one
                 :db/doc (str "A " schema-name "'s " attribute-name)
-                :db/install/_attribute :db.part/db}])
+                :db.install/_attribute :db.part/db}])
             fields)))
-
