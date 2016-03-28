2e8605551eb86a82eda271147ade8f60feae1648
Sergiy BONDARYEV <sbondaryev@gmail.com>
2016-03-28 23:19:18 +0200

    change clojure verstion to pass tests

--- datomic-dsl/project.clj
+++ datomic-dsl/project.clj
@@ -1,6 +1,6 @@
 (defproject datomic-dsl "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.datomic/datomic-free "0.9.5350" :exclusions [joda-time]]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.datomic/datomic-free "0.9.5130" :exclusions [joda-time]]]
   :main ^:skip-aot datomic-dsl.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
