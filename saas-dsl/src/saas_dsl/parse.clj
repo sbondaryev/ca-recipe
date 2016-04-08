641759b40c83d32b5db6df188a2325eeb787d73f
Sergiy BONDARYEV <sbondaryev@gmail.com>
2016-04-08 23:22:49 +0200

    get-lines function

--- saas-dsl/src/saas_dsl/parse.clj
+++ saas-dsl/src/saas_dsl/parse.clj
@@ -1,3 +1,9 @@
 (ns sass-dsl.parse
   (:require [clojure.zip :as zip]
             [clojure.string :refer [split] :as str]))
+
+(defn get-lines
+  "Tokenise the input into lines."
+  [input-str]
+  (:pre [(= (class input-str) java.lang.String)])
+  (split input-str #"\n+"))
