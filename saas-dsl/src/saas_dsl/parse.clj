42e0c33c98e260d66e45766a8f9b3114d58641a6
Sergiy BONDARYEV <sbondaryev@gmail.com>
2016-04-09 23:22:40 +0200

    indent functions

--- saas-dsl/src/saas_dsl/parse.clj
+++ saas-dsl/src/saas_dsl/parse.clj
@@ -7,3 +7,13 @@
   [input-str]
   (:pre [(= (class input-str) java.lang.String)])
   (split input-str #"\n+"))
+
+(defn count-space-indent
+  "Count the number of spaces used to indent."
+  [input-string]
+  (count (take-while #{\space} input-string)))
+
+(defn has-indent
+  "Does this line has an indent?"
+  [loc]
+  (pos? (count-space-indent (zip/node loc))))
