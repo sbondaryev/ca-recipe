34d607364090a9ed057b9821993b0102a76c54ff
Sergiy BONDARYEV <sbondaryev@gmail.com>
2016-04-17 23:22:42 +0200

    export mains

--- worker/src/worker/core.cljs
+++ worker/src/worker/core.cljs
@@ -2,7 +2,11 @@
   (:require [cljs.js]
             [worker.worker :as w]))
 
+
 (defn ^:export main []
   (enable-console-print!)
-  (println "start..")
-  (w/do-some))
+  (println "work1..")
+  (w/do-some `wrk)
+  (println "work2..")
+  (w/do-some `wrk1)
+  )
