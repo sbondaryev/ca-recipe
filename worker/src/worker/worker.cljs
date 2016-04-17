d7d7f50604d10ca4702fdb417aa59c01c14fcf6c
Sergiy BONDARYEV <sbondaryev@gmail.com>
2016-04-17 23:33:25 +0200

    rewrite worker-body to work with different compile modes

--- worker/src/worker/worker.cljs
+++ worker/src/worker/worker.cljs
@@ -2,58 +2,32 @@
   :getElementsByTagName "function() {return [];}"
 })
 
+(defn worker-body [[ns* fn*]]
+  (let [
+    multi-loader (str
+     "var CLOSURE_BASE_PATH = '" *closure-base-path* "';"
+     "var CLOSURE_IMPORT_SCRIPT = (function(global) {"
+     "return function(src) {"
+        ;;"global['console'].log(src);"
+        "global['importScripts'](src);"
+        "return true;"
+     "};"
+     "})(self);"
+     "importScripts('" *closure-base-file* "','" *cljs-output-file* "');"
+     "goog.require('" ns* "');")
+    single-loader (str
+      "importScripts('" *cljs-output-file* "');"
+    )]
   (str
     "var document=" (serialize *document*) ";"
-   "var CLOSURE_BASE_PATH = '" closure-base-path "';"
-   "var CLOSURE_IMPORT_SCRIPT = (function(global) {"
-   "return function(src) {"
-      ;;"global['console'].log(src);"
-      "global['importScripts'](src);"
-      "return true;"
-   "};"
-   "})(self);"
-   "importScripts('" closure-base-file "','" cljs-deps-file "');"
-   "goog.require('worker.worker');"
+    (if (empty? *closure-base-path*) single-loader multi-loader)
+    "self.onmessage = function(e) {"
+      ns* "." fn* ".apply();"
+    "};")))
 
-   "self.onmessage = function(e) {"
-     "worker.worker.wrk();"
-   "};"))
+(defn full-func-name [wrk]
+  (js->clj (.split (str wrk) "/")))
 
-(defn worker-body-min [fn-name]
- (str
-  "importScripts('file:///Users/sbondaryev/clojure/sandbox/worker/resources/public/js/compiled/worker-min.js');"
-  "self.onmessage = function(e) {"
-    "worker.worker.wrk();"
-  "};"))
-
-
-(defn func-name [f]
-  (let [fstr (str f)
-        fname (subs fstr (count "function ") (.indexOf fstr "("))
-        fsplit (.split fname "$")]
-    (.join fsplit ".")))
-
-
-(defn ^:export wrk []
-  (enable-console-print!)
-  (println tm)
-  (sleep tm)
-  (println "test")
-  ;;(throw (js/Error. "test"))
-  )
-
-
-(defn do-some []
-(println goog.basePath)
-  (let [a (worker-body (func-name wrk))]
-    (println a)
-
-    (def b (js/Blob. (clj->js [a])))
-
-    (def w (js/Worker. (.createObjectURL js/URL b)))
-
-    (.postMessage w nil)
-    ))
 (defn do-some [wrk]
   (let [a (worker-body (full-func-name wrk))
         b (js/Blob. (clj->js [a]))
