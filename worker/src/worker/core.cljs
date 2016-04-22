9c42928da85c62c250e3cf2121f511c080b6822d
Sergiy BONDARYEV <sbondaryev@gmail.com>
2016-04-22 21:48:20 +0200

    function to create worker

--- worker/src/worker/core.cljs
+++ worker/src/worker/core.cljs
@@ -25,12 +25,26 @@
   {:time tm
     :prnt 100})
 
+(defn do-some []
+  (let [w (js/Worker. (.createObjectURL js/URL worker.worker/worker-blob))
+        wmeta (meta (var wrk))
+        result (atom)]
+    (set! (.-onmessage w) #(reset! result (worker.worker/*deserialize* (.-data %))))
+    (.postMessage w (cljs.core/clj->js [(:ns wmeta) (:name wmeta)]))
+    {:w w :result result}))
+
 (defn ^:export main []
   (enable-console-print!)
-  (println "work1..")
+  (def some-worker (do-some))
+
+  (println some-worker)
+  (js/setTimeout #(do (sleep 2000) (println some-worker)), 3000)
+
+  ;;(println some-worker)
+
 ;;  (println (w/js-func-name wrk))
 ;;  (wm/do-some 1)
-    (w/do-some (meta (var wrk)))
+;;    (w/do-some (meta (var wrk)))
 ;;  (println "work2..")
 ;;  (w/do-some `wrk2)
   )
