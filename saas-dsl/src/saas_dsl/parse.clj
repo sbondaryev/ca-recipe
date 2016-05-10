e5a623c2d5f3d2d83a5db66b5d7b722c483e5b38
Sergiy BONDARYEV <sbondaryev@gmail.com>
2016-05-10 23:52:47 +0200

    add branch check

--- saas-dsl/src/saas_dsl/parse.clj
+++ saas-dsl/src/saas_dsl/parse.clj
@@ -19,7 +19,14 @@
   (pos? (count-space-indent (zip/node loc))))
 
 (defn drop-one-indent
-  "Shuffle this down an indent by stripping two leading spaces.")
+  "Shuffle this down an indent by stripping two leading spaces."
   [input-string]
   (:pre [(and (= (.charAt input-string 0) \space) (= (.charAt input-string 1) \space))])
   (subs input-string 2))
+
+(defn is-left-branch
+  "Is there a branch to the left?"
+  [loc]
+  {:pre [(not (nil? loc))]}
+  (if (zip/left loc)
+    (zip/branch? (zip/left loc))))
