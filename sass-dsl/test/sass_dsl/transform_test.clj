4aa97b7781bee8e5799932c4dd9feb43741ae4fa
Sergiy BONDARYEV <sbondaryev@gmail.com>
2016-06-22 23:16:59 +0200

    transform test (initial commit)

---
+++ sass-dsl/test/sass_dsl/transform_test.clj
@@ -0,0 +1,8 @@
+(ns sass-dsl.transform-test
+  (:require [clojure.test :refer :all]
+            [sass-dsl.parse :as parse]
+            [sass-dsl.transform :as transform]
+            [sass-dsl.common-test :as common-test]
+            [sass-dsl.parse-test :as parse-test]
+            [sass-dsl.common-test :as common-test]))
+            
