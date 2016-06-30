243d4dfe570ac96f8eb0f1306fe88f4abd3b30a5
Sergiy BONDARYEV <sbondaryev@gmail.com>
2016-06-30 23:28:19 +0200

    help funcs

--- stuf/src/sandbox/deriv.clj
+++ stuf/src/sandbox/deriv.clj
@@ -1,25 +1,34 @@
 (ns sandbox.deriv)
 
-(defn deriv [exp var]
-  (cond ((number? exp) 0)
-        ((variable? exp)
-         (if (same-variable? exp var) 1 0))
-        ((sum? exp)
-         (make-sum (deriv (addend exp) var)
-                   (deriv (augend exp) var)))
-        ((product? exp)
-         (make-sum
-          (make-product (multiplier exp)
-                        (deriv (multiplicated exp) var))
-          (make-product (deriv (multiplier exp) var)
-                        (multiplicand exp))))
-        (else
-         (println "unknown expresion type -- DERIV" exp))))
-
-
-
 (defn variable? [x] (symbol? x))
 (defn same-variable? [v1 v2]
   (and (variable? v1) (variable? v2) (= v1 v2)))
+
 (defn make-sum [a1 a2] (list '+ a1 a2))
 (defn make-product [a1 a2] (list '* a1 a2))
+
+(defn sum? [[x :as s]]
+  (and (seq s) (= x '+)))
+(defn addend [[_ x]] x)
+(defn augend [[_ _ x]] x)
+
+(defn product? [[x :as p]]
+  (and (seq p) (= x '*)))
+(defn multiplier [[_ p]] p)
+(defn multiplicand [[_ _ p]] p)
+
+(defn deriv [exp var]
+  (cond (number? exp) 0
+        (variable? exp)
+         (if (same-variable? exp var) 1 0)
+        (sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var))
+        (product? exp)
+         (make-sum
+          (make-product (multiplier exp)
+                        (deriv (multiplicand exp) var))
+          (make-product (deriv (multiplier exp) var)
+                        (multiplicand exp)))
+        :else
+         (println "unknown expresion type -- DERIV" exp)))
